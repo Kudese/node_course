39255118e1904d74b9aac0302651a8d8d3ea16e8
HakuMatsunoki <hakumatsunoki@gmail.com>
2023-03-15 23:23:38 +0200

    lesson 07

---
+++ 07/middlewares/authMiddleware.js
@@ -0,0 +1,55 @@
+const jwt = require('jsonwebtoken');
+
+const { AppError, catchAsync, validators } = require('../utils');
+const User = require('../models/userModel');
+
+/**
+ * Check signup user data.
+ */
+exports.checkSignupData = catchAsync(async (req, res, next) => {
+  const { error, value } = validators.signupUserValidator(req.body);
+
+  if (error) return next(new AppError(400, error.details[0].message));
+
+  const { email } = value;
+
+  const userExists = await User.exists({ email });
+
+  if (userExists) return next(new AppError(409, 'User with this email already exists..'));
+
+  req.body = value;
+
+  next();
+});
+
+/**
+ * Middleware to allow only login users.
+ */
+exports.protect = catchAsync(async (req, res, next) => {
+  // Extract token from authorization header
+  const token = req.headers.authorization?.startsWith('Bearer') && req.headers.authorization.split(' ')[1];
+
+  if (!token) return next(new AppError(401, 'You are not logged in..'));
+
+  // Use async version of token verification
+  const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+
+  // Find user by id decoded from token
+  const currentUser = await User.findById(decodedToken.id);
+
+  req.user = currentUser;
+
+  next();
+});
+
+/**
+ * Roles guard. Restrict access to specific role(s).
+ * Use it only after 'protect' middleware!
+ * @param  {...string} roles
+ */
+exports.allowFor = (...roles) =>
+  (req, res, next) => {
+    if (roles.includes(req.user.role)) return next();
+
+    next(new AppError(403, 'You are not allowed to perform this action..'));
+  };
---
+++ 07/models/userModel.js
@@ -0,0 +1,52 @@
+const { model, Schema } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const { enums } = require('../constants');
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: [true, 'User with this email already exists..'],
+      lowercase: true,
+    },
+    birthyear: {
+      type: Number,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: Object.values(enums.USER_ROLES_ENUM),
+      default: enums.USER_ROLES_ENUM.USER,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Mongoose pre-save hook. In case of using this keyword, don't use arrow functions as callback!!
+userSchema.pre('save', async function(next) {
+  // Passwords auto-hashing
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+// Mongoose custom method
+userSchema.methods.checkPassword = (candidate, hash) => bcrypt.compare(candidate, hash);
+
+const User = model('User', userSchema);
+
+module.exports = User;
